ee9d6e6d293949af18ecb0aa923376b8f6d194dd
ayaannasim9 <ayaannasim9@gmail.com>
2025-11-17 00:38:58 +0000

    Added implementation for flags, and byte count, restructured code a little bit

--- main.py
+++ main.py
@@ -1,42 +1,96 @@
 import sys
+import os
 argument=sys.argv[1:]
 files=[]
 flags=[]
-# print(files)
-# flags={
-#     'l':"line_count",
-#     'w':"word_count",
-#     'c':"character_count",
-#     'm':"byte_count"
-# }
+line_count=False
+word_count=False
+byte_count=False
+character_count=False
+
 def handle_input():
     if not argument:
         return False
+    flags_empty=True
     for arg in argument:
         if arg.startswith("-"):
             flags.append(arg[1:])
+            flags_empty=False
         else:
             files.append(arg)
+            if flags_empty:
+                return False
     return True
-def word_count():
+
+def handle_flags():
+    global line_count,word_count,byte_count,character_count
+    if not flags:
+        line_count=True
+        word_count=True
+        byte_count=True
+        return
+    for flag in flags:      
+        for c in flag.lower():
+            if c=="l":
+                line_count=True
+            elif c=="w":
+                word_count=True
+            elif c=="c":
+                byte_count=True
+            elif c=="m":
+                character_count=True
+            else:
+                continue    
+
+def word_count_imp():
     result=[]
     for file in files:
-        lines=0
-        words=0
-        characters=0
         try:
             with open(file,"r") as f:
                 all_lines=f.readlines()
-                for line in all_lines:
-                    lines+=1
-                    words+=len(line.split())
-                    characters+=len(line)
-                result.append([f"{lines} {words} {characters} {file}"])
+                result_string=[]
+                if line_count:
+                    lines=line_counter(all_lines)
+                    result_string.append(str(lines))
+                if word_count:
+                    words=word_counter(all_lines)
+                    result_string.append(str(words))
+                if character_count:
+                    characters=character_counter(all_lines)
+                    result_string.append(str(characters))
+                if byte_count:
+                    byte_count_val=byte_counter(f)
+                    result_string.append(str(byte_count_val))
+                result_string.append(file)
+                result.append([" ".join(result_string)])
         except FileNotFoundError as not_found:
             result.append([not_found.filename])
     return result
+
+def line_counter(all_lines):
+    # will be implemented if line_count is set to True
+    # returns the number of lines
+    return len(all_lines)
+
+def word_counter(all_lines):
+    # is only called when word_count is set to True
+    # returns the number of words
+    words=0
+    for line in all_lines:
+        words+=len(line.split())
+    return words
+
+def character_counter(all_lines):
+    characters=0
+    for line in all_lines:
+        characters+=len(line)
+    return characters
+
+def byte_counter(file):
+    return os.path.getsize(file.name)
+
 def print_result():
-    result=word_count()
+    result=word_count_imp()
     for res in result:
         if len(res[0].split())==1:
             print(f"{res[0]} was not found")
@@ -44,8 +98,7 @@ def print_result():
             print(res[0])
 if __name__=="__main__":
     if not handle_input():
-        print("You have not entered any file")
+        print("Invalid Input")
         quit()
-    print(flags)
-    print(files)
+    handle_flags()
     print_result()
